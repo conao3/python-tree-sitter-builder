fa8c1c1e2396b101324820978c3b318978703ab9
Naoya Yamashita <conao3@gmail.com>
2023-07-20 23:42:49 +0900

    build all modules if no argument

--- src/tree_sitter_builder/__main__.py
+++ src/tree_sitter_builder/__main__.py
@@ -22,19 +22,20 @@ def main_build(args: argparse.Namespace):
     repo_dir = lib.dir.get_repo_dir(data_dir)
     build_dir = lib.dir.get_build_dir(data_dir)
 
-    logger.info(f'Clone `{args.repository}` to `{repo_dir}`')
-    clone_dir_name = subr.git.get_repo_name(args.repository)
-    subr.git.clone(
-        url=subr.git.get_repo_url(args.repository),
-        dir=repo_dir,
-        name=clone_dir_name,
-    )
+    if args.repository:
+        logger.info(f'Clone `{args.repository}` to `{repo_dir}`')
+        clone_dir_name = subr.git.get_repo_name(args.repository)
+        subr.git.clone(
+            url=subr.git.get_repo_url(args.repository),
+            dir=repo_dir,
+            name=clone_dir_name,
+        )
+        lib.main.build(args.repository, repo_dir, build_dir)
+        return
 
-    logger.info(f'Build tree-sitter module: {str(build_dir / (clone_dir_name + ".so"))}')
-    tree_sitter.Language.build_library(
-        str(build_dir / (clone_dir_name + '.so')),
-        [str(repo_dir / clone_dir_name)]
-    )
+    for repository in repo_dir.iterdir():
+        logger.info(f'Build {repository.name}')
+        lib.main.build(repository.name, repo_dir, build_dir)
 
 
 def main_update(args: argparse.Namespace):
@@ -46,7 +47,7 @@ def parse_args() -> argparse.Namespace:
     subparsers = parser.add_subparsers(dest='command')
 
     parser_build = subparsers.add_parser('build', help='Build tree-sitter module.')
-    parser_build.add_argument('repository', help='Repository specifier in github or git URL.')
+    parser_build.add_argument('repository', help='Repository specifier in github or git URL.', nargs='?')
     parser_build.set_defaults(handler=main_build)
 
     parser_update = subparsers.add_parser('update', help='Update tree-sitter module.')
