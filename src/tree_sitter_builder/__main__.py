2a6e835ea2a5f8f9d1e02b635430e770f51a88e6
Naoya Yamashita <conao3@gmail.com>
2023-07-20 22:51:33 +0900

    add clone feature

--- src/tree_sitter_builder/__main__.py
+++ src/tree_sitter_builder/__main__.py
@@ -1,5 +1,9 @@
 import argparse
+import pathlib
 
+import platformdirs
+
+from . import subr
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -10,3 +14,15 @@ def parse_args() -> argparse.Namespace:
 def main():
     args = parse_args()
     print(args)
+
+    data_dir = pathlib.Path(platformdirs.user_data_dir('tree-sitter-builder'))
+    data_dir.mkdir(parents=True, exist_ok=True)
+
+    repo_dir = data_dir / 'repos'
+    repo_dir.mkdir(parents=True, exist_ok=True)
+
+    subr.git.clone(
+        url=subr.git.get_repo_url(args.repository),
+        dir=repo_dir,
+        name=subr.git.get_repo_name(args.repository),
+    )
