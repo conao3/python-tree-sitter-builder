bdbcaabec99305d4f349f622c09a805a24d35982
Naoya Yamashita <conao3@gmail.com>
2023-07-20 22:03:11 +0900

    argparse

--- src/tree_sitter_builder/__main__.py
+++ src/tree_sitter_builder/__main__.py
@@ -1,2 +1,12 @@
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('repository', help='Repository specifier in github or git URL.')
+
+    return parser.parse_args()
+
 def main():
-    print('Hello, world!')
+    args = parse_args()
+    print(args)
