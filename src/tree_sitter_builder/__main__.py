4af1d4c9a93ae0a74ec352d877c3999de06c66cd
Naoya Yamashita <conao3@gmail.com>
2023-07-20 23:02:53 +0900

    add logger

--- src/tree_sitter_builder/__main__.py
+++ src/tree_sitter_builder/__main__.py
@@ -1,15 +1,27 @@
+import logging
+import logging.config
 import argparse
 import pathlib
 
 import platformdirs
+import yaml
 
 from . import subr
+
+
+with open(pathlib.Path(__file__).parent / 'logging.conf.yml') as f:
+    logging.config.dictConfig(yaml.safe_load(f))
+
+
+logger = logging.getLogger(__name__)
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('repository', help='Repository specifier in github or git URL.')
 
     return parser.parse_args()
+
 
 def main():
     args = parse_args()
@@ -20,6 +32,7 @@ def main():
     repo_dir = data_dir / 'repos'
     repo_dir.mkdir(parents=True, exist_ok=True)
 
+    logger.info(f'Clone `{args.repository}` to `{repo_dir}`')
     subr.git.clone(
         url=subr.git.get_repo_url(args.repository),
         dir=repo_dir,
